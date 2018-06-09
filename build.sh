8e6f89a1b5ae5c7cb8b78c4159d1b419e2c7a93a
brinkqiang <brinkqiang@users.noreply.github.com>
2018-06-09 13:05:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yum install git
-yum -y install gettext gcc autoconf libtool automake make asciidoc xmlto c-ares-devel libev-devel mbedtls-devel libsodium-devel pcre-devel
+yum install gettext gcc autoconf libtool automake make asciidoc xmlto c-ares-devel libev-devel mbedtls-devel libsodium-devel pcre-devel
 
 sudo libtoolize && sudo aclocal && sudo autoheader && sudo autoconf && sudo automake --add-missing
 
